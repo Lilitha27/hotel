c3126a112227623ea9a61fc72a3a586ecce42b4c
Lilitha27 <lilithakganya@gmail.com>
2019-09-17 13:31:52 +0200

    added another form

--- index.php
+++ index.php
@@ -22,40 +22,47 @@ session_start();
         hotelname VARCHAR(50),
         indate VARCHAR(30),
         outdate VARCHAR(30),
-        booked INT(4)
+      
     )";
 
     $conn->query($sql);
     echo $conn->error;
     ?>
 
-<div class="container">
-        <div class="row">
-            <div class="col" >
-                <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
-                <div class="form-group">
+<h1>Las Vegas hotel booking</h1>
+<main class="container hotel">
+
+  
+                <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post" class="form">
+                <div class="form-group col-md-6">
                         <label for="firstName">First name</label>
                         <input type="text" class="form-control" placeholder="First name" name="firstname" required>
                 </div>
                 <br>
-                <div class="form-group">
+                <div class="form-group col-md-6">
                         <label for="surname">Last name</label>
                         <input type="text" class="form-control" placeholder="Last name" name="surname" required>
                 </div>
                 <br>
+                <div class="form-group col-md-6">
                 <label for="hotels">Select a hotel below :</label>
                 <select class="form-control form-control-lg" name="hotelname" id="hotelName" required>
-                        <option value="The Orleans">The Orleans </option>
-                        <option value="Bellagio">Bellagio </option>
-                        <option value="The Four Queens">The Four Queens</option>
-                        <option value="The Cosmopolitan">The Cosmopolitan </option>
+                        <option value=" ">  </option>
+                        <option value="The Orleans - R700 per night">The Orleans - R700 per night</option>
+                        <option value="Bellagio - R600 per night">Bellagio - R600 per night </option>
+                        <option value="The Four Queens - R700 per night">The Four Queens - R700 per night</option>
+                        <option value="The Cosmopolitan - R800 per night">The Cosmopolitan - R800 per night </option>
                         
                 </select>
+                </div>
                 <br>
                 <br>
+                <div class="form-group col-md-6">
+                <label for="start">Start date :</label>
                 <input type="date" class="form-control" name="indate" min="2018-01-01" max="2020-12-31" aria-label="Todo Date" aria-describedby="button-addon2">
+                <label for="end">End date :</label>
                 <input type="date" class="form-control" name="outdate" min="2018-01-01" max="2020-12-31" aria-label="Todo Date" aria-describedby="button-addon2">
-
+             </div>
                 <!-- <div class="form-group">
                         <label for="numberOfDays">NUMBER OF DAYS</label>
                         <input type="number" class="form-control" placeholder="Number Of Days" min=1 name="numberOfDays" id="nums" required>
@@ -65,11 +72,9 @@ session_start();
                 </form>
                 <br>
                 
-           
-            </div>
-           
-        </div>
-    </div>
+        
+</main>       
+       
 
     </body>
 </html>
