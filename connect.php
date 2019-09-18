ba74ce265fe8d2122a90b51d1bb21f83e7394d69
Lilitha27 <lilithakganya@gmail.com>
2019-09-18 08:59:58 +0200

    added a connection page

---
+++ connect.php
@@ -0,0 +1,9 @@
+<?php
+require_once 'base.php';
+
+$conn = new mysqli('localhost','root','root','hotel');
+
+if ($conn->connect_error){
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
